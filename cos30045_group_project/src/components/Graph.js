9e4be91d7d27901152d7c0e670948d55fb9c6b50
cj-travis <118725336+cj-travis@users.noreply.github.com>
2024-11-16 00:23:18 +0800

    graph styling
    - added legend
    - styling adjustments

--- cos30045_group_project/src/components/Graph.js
+++ cos30045_group_project/src/components/Graph.js
@@ -12,10 +12,10 @@ export default function Graph() {
       setSelectedDataset(event.target.value);
     };
 
-    var padding_x = 100; // padding
+    var padding_x = 120; // padding
     var padding_y = 50;
     var w = 1000; // width
-    var h = 500; // height
+    var h = 400; // height
 
   useEffect(() => {
     // If it's not the first render, do nothing
@@ -28,22 +28,28 @@ export default function Graph() {
     // Function to load data based on selection
     const loadData = (datasetType) => {
       switch (datasetType) {
-        case 'population':
-          return d3.csv('/datasets/population.csv');
-        case 'respiratory':
-          return d3.csv('/datasets/respiratory_death_rate.csv');
-        case 'cardiovascular':
-          return d3.csv('/datasets/cardiovascular_death_rate.csv');
+        case 'Population':
+          return { data: d3.csv('/datasets/population.csv'), color: '#D3D3D3', title: datasetType};
+        case 'Respiratory Death Rate':
+          return { data: d3.csv('/datasets/respiratory_death_rate.csv'), color: '#D3D3D3', title: datasetType};
+        case 'Cardiovascular Death Rate':
+          return { data: d3.csv('/datasets/cardiovascular_death_rate.csv'), color: '#D3D3D3', title: datasetType};
         default:
-          return Promise.reject('Invalid dataset selected');
+          // return Promise.reject('Invalid dataset selected');
+          return { data: d3.csv('/datasets/population.csv'), color: '#D3D3D3', title: 'Population'}
       }
     };
 
     // Load the CSV file and process the data
     d3.csv('/datasets/oecd_pm25_exposure.csv').then(function(data_oecd) {
-      loadData(selectedDataset).then((data_sub) => {
+      const { data, color, title } = loadData(selectedDataset);
 
-        setDataset({ oecd: data_oecd, sub: data_sub });
+      data.then((data_sub) => {
+
+        console.log(data_sub); // process the dataset
+        console.log(color); // use the color
+
+        setDataset({ oecd: data_oecd, sub: data_sub, color: color, title: title });
 
     }).catch((error) => {
       console.error('Error loading data:', error);
@@ -62,7 +68,7 @@ export default function Graph() {
     d3.select('#graph').selectAll('*').remove();
 
     // Extracting and processing data for rendering
-    const { oecd, sub } = dataset;
+    const { oecd, sub, color, title } = dataset;
 
     // Find the row for "Japan"
     const country_oecd = oecd.find(d => d['Reference area'] === 'Australia');
@@ -133,6 +139,19 @@ export default function Graph() {
                 .x(function(d) { return xScale(d.sub_year); })
                 .y(function(d) { return yScale_right(d.sub_value); });
 
+    d3.select('#sub-legend-line')
+                    .append('rect')
+                    .attr("width", "100%")
+                    .attr("height", "100%")
+                    .attr("fill", color);
+
+    d3.select('#sub-legend-title')
+            .selectAll('*').remove();
+
+    d3.select('#sub-legend-title')
+                    .append('text')
+                    .text(title);
+
     // create a svg element in the body tag
     var svg = d3.select("#graph")
         .append("svg")
@@ -144,7 +163,7 @@ export default function Graph() {
         .attr("class", "line")
         .attr("d", line)
         .attr("fill", "none")
-        .attr("stroke", "slategrey") 
+        .attr("stroke", "#00FFFF") 
         .attr("stroke-width", 0.5);
 
     var path_sub = svg.append("path")
@@ -152,7 +171,7 @@ export default function Graph() {
         .attr("class", "line")
         .attr("d", line_sub)
         .attr("fill", "none") 
-        .attr("stroke", "red")
+        .attr("stroke", color)
         .attr("stroke-width", 0.5);
 
     // Get the total length of the path
@@ -166,7 +185,7 @@ export default function Graph() {
 
     // Animate the line by transitioning stroke-dashoffset to 0
     path.transition()
-        .duration(2000)
+        .duration(1000)
         .ease(d3.easeLinear) 
         .attr("stroke-dashoffset", 0);
     
@@ -176,7 +195,7 @@ export default function Graph() {
       .attr("stroke-dashoffset", totalLength2)
 
     path_sub.transition()
-        .duration(2000) 
+        .duration(1000) 
         .ease(d3.easeLinear)  
         .attr("stroke-dashoffset", 0);
 
@@ -188,10 +207,12 @@ export default function Graph() {
     // display the y axis in the svg element
     svg.append("g")
         .attr("transform", "translate(" + (padding_x) + ", 0)")
+        .style("color", "#00FFFF")
         .call(yAxis);
 
     svg.append("g")
         .attr("transform", "translate(" + (w - padding_x) + ", 0)")
+        .style("color", color)
         .call(yAxis_right);
 
     // Add points to the line (optional, for better visibility)
@@ -202,13 +223,14 @@ export default function Graph() {
       .attr("cx", function(d) { return xScale(d.year); })
       .attr("cy", function(d) { return yScale(d.value); })
       .attr("r", 2.5)
-      .attr("fill", "slategrey")
+      .style("z-index", 98) 
+      .attr("fill", "#00FFFF")
       .attr("opacity", 0);
 
     // Animate the dots: fade them in after the line animation
     dots.transition()
-      .duration(800)
-      .delay(function(d, i) { return 800 + i * 50; })  
+      .duration(500)
+      .delay(function(d, i) { return 500 + i * 20; })  
       .attr("opacity", 1); 
 
     dots.on("mouseover", function(event, d) {
@@ -229,31 +251,54 @@ export default function Graph() {
             .attr("transform", "translate(" + xPosition + "," + yPosition + ")");
 
         tooltip.append("rect")
-          .attr("x", 5)  
-          .attr("y", -30)  
+          .attr("x", -110)  
+          .attr("y", -60)  
           .attr("width", 120)  
           .attr("height", 50)  
-          .attr("fill", "rgba(0, 0, 0, 0.7)")
+          .attr("fill", "rgba(0, 0, 0, 1)")
           .attr("rx", 4)  
           .attr("ry", 4) 
           .style("stroke", "white") 
-          .style("stroke-width", 1);  
+          .style("z-index", 99) 
+          .style("stroke-width", 0);  
 
         tooltip.append("text")
-            .attr("x", 10)  
-            .attr("y", -10) 
+            .attr("x", -100)  
+            .attr("y", -36) 
             .attr("text-anchor", "start")
             .attr("font-size", "12px")
             .attr("fill", "white")
             .text("Year: " + d.year);
 
         tooltip.append("text")
-            .attr("x", 10)
-            .attr("y", 5)  
+            .attr("x", -100)
+            .attr("y", -24)  
             .attr("text-anchor", "start")
             .attr("font-size", "12px")
             .attr("fill", "white")
             .text("Value: " + d.value);
+
+        // Add dotted line to X axis
+        svg.append("line")
+          .attr("x1", xPosition)
+          .attr("y1", yPosition + 5)
+          .attr("x2", xPosition)
+          .attr("y2", yScale.range()[0]) // Y axis position
+          .attr("stroke", "grey")
+          .attr("z-index", 0)
+          .style("stroke-dasharray", "4,4") // Dotted line style
+          .attr("stroke-width", 1);
+
+        // Add dotted line to Y axis
+        svg.append("line")
+            .attr("x1", xPosition - 5)
+            .attr("y1", yPosition)
+            .attr("x2", xScale.range()[0]) // X axis position
+            .attr("y2", yPosition)
+            .attr("stroke", "grey")
+            .attr("z-index", 0)
+            .attr("stroke-dasharray", "4,4") // Dotted line style
+            .attr("stroke-width", 1);
     })
     .on("mouseout", function() {
         // Reset circle color back to original and remove the tooltip
@@ -261,10 +306,13 @@ export default function Graph() {
             .transition()
             .duration(600)
             .attr("r", 2.5)
-            .attr("fill", "slategrey");
+            .attr("fill", "#00FFFF");
 
         // Remove tooltip
         d3.select("#tooltip").remove();
+
+        // Remove the dotted lines
+        svg.selectAll("line").remove();
     });
 
     var dots2 = svg.selectAll(".dot2")
@@ -274,13 +322,14 @@ export default function Graph() {
       .attr("cx", function(d) { return xScale(d.sub_year); })
       .attr("cy", function(d) { return yScale_right(d.sub_value); })
       .attr("r", 2.5)
-      .attr("fill", "red")
+      .attr("fill", color)
+      .style("z-index", 98) 
       .attr("opacity", 0);
 
     // Animate the dots: fade them in after the line animation
     dots2.transition()
-      .duration(800) 
-      .delay(function(d, i) { return 800 + i * 50; })  
+      .duration(500) 
+      .delay(function(d, i) { return 500 + i * 20; })  
       .attr("opacity", 1); 
 
     dots2.on("mouseover", function(event, d) {
@@ -300,32 +349,55 @@ export default function Graph() {
             .attr("id", "tooltip")
             .attr("transform", "translate(" + xPosition + "," + yPosition + ")");
 
-        tooltip.append("rect")
-          .attr("x", 5) 
-          .attr("y", -30) 
-          .attr("width", 120)  
-          .attr("height", 50) 
-          .attr("fill", "rgba(0, 0, 0, 0.7)") 
-          .attr("rx", 4)  
-          .attr("ry", 4)  
-          .style("stroke", "white") 
-          .style("stroke-width", 1); 
+          tooltip.append("rect")
+            .attr("x", -110)  
+            .attr("y", -60)  
+            .attr("width", 120)  
+            .attr("height", 50)  
+            .attr("fill", "rgba(0, 0, 0, 1)")
+            .attr("rx", 4)  
+            .attr("ry", 4) 
+            .style("stroke", "white") 
+            .style("z-index", 99) 
+            .style("stroke-width", 0);  
+  
+          tooltip.append("text")
+              .attr("x", -100)  
+              .attr("y", -36) 
+              .attr("text-anchor", "start")
+              .attr("font-size", "12px")
+              .attr("fill", "white")
+              .text("Year: " + d.sub_year);
+  
+          tooltip.append("text")
+              .attr("x", -100)
+              .attr("y", -24)  
+              .attr("text-anchor", "start")
+              .attr("font-size", "12px")
+              .attr("fill", "white")
+              .text("Value: " + d.sub_value);
 
-        tooltip.append("text")
-            .attr("x", 10)  
-            .attr("y", -10) 
-            .attr("text-anchor", "start")
-            .attr("font-size", "12px")
-            .attr("fill", "white")
-            .text("Year: " + d.sub_year);
+        // Add dotted line to X axis
+        svg.append("line")
+          .attr("x1", xPosition)
+          .attr("y1", yPosition + 5)
+          .attr("x2", xPosition)
+          .attr("y2", yScale_right.range()[0]) // Y axis position
+          .attr("stroke", "grey")
+          .style("z-index", 0)
+          .attr("stroke-dasharray", "4,4") // Dotted line style
+          .attr("stroke-width", 1);
 
-        tooltip.append("text")
-            .attr("x", 10)
-            .attr("y", 5) 
-            .attr("text-anchor", "start")
-            .attr("font-size", "12px")
-            .attr("fill", "white")
-            .text("Value: " + d.sub_value);
+        // Add dotted line to Y axis
+        svg.append("line")
+            .attr("x1", xPosition + 5)
+            .attr("y1", yPosition)
+            .attr("x2", xScale.range()[1]) // X axis position
+            .attr("y2", yPosition)
+            .attr("stroke", "grey")
+            .style("z-index", 0)
+            .attr("stroke-dasharray", "4,4") // Dotted line style
+            .attr("stroke-width", 1);
     })
     .on("mouseout", function() {
         // Reset circle color back to original and remove the tooltip
@@ -333,42 +405,64 @@ export default function Graph() {
             .transition()
             .duration(600)
             .attr("r", 2.5)
-            .attr("fill", "red");
+            .attr("fill", color);
 
         // Remove tooltip
         d3.select("#tooltip").remove();
+
+        // Remove the dotted lines
+        svg.selectAll("line").remove();
     });
   }, [dataset]);
 
   return (
     <section 
-      className="relative w-full min-h-dvh bg-gradient-to-b from-gray-900 to-gray-800 flex items-center justify-center scroll-section"
+      className="relative w-full min-h-dvh text-white/70 flex items-center justify-center scroll-section"
       id="visual"
     >
       <div className="relative">
-        <h2 className="text-white w-[100%] text-center text-xl">Country</h2>
+        <h2 className="text-white/70 w-[100%] text-center text-xl">Country</h2>
         <div className="w-full flex items-center absolute translate-y-4">
-          <span className="w-[20%] text-white text-xs bg-transparent ml-0 mr-auto">
+          <span className="w-[20%] text-white/70 text-xs bg-transparent ml-0 mr-auto">
             PM2.5 Exposure Level
           </span>
           <select
-            className="w-[20%] text-white text-xs bg-transparent hover:border rounded transition duration-150"
+            className="w-[20%] text-white/70 text-xs bg-transparent hover:border rounded transition duration-150"
             value={selectedDataset}
             onChange={handleChange}
           >
-            <option className="text-black" value="population">
+            <option className="text-black" value="Population">
               Population
             </option>
-            <option className="text-black" value="respiratory">
+            <option className="text-black" value="Respiratory Death Rate">
               Respiratory Death Rate
             </option>
-            <option className="text-black" value="cardiovascular">
+            <option className="text-black" value="Cardiovascular Death Rate">
               Cardiovascular Death Rate
             </option>
           </select>
         </div>
         
         <div id="graph"></div>
+
+        <div className='w-full text-center absolute -translate-y-4'><span>Year</span></div>
+
+        <div class="w-full flex items-center justify-center space-x-6 mt-12">
+          <div className='w-fit flex items-center space-x-2'>
+            <svg width="30" height="2" class="">
+              <rect width="100%" height="100%" fill="#00FFFF" />
+            </svg>
+            <span class="text-sm">PM2.5 Exposure Level</span>
+          </div>
+
+          <div className='w-fit flex items-center space-x-2'>
+            <svg id='sub-legend-line' width="30" height="2" class="">
+              <rect  width="100%" height="100%" fill="#00FFFF" />
+            </svg>
+            <span id='sub-legend-title' class="text-sm"></span>
+          </div>
+          
+        </div>
       </div>
     </section>
   );
